0617a5c1191fb6e412d532441fbaf5ed573f1028
kimywann <Kimywan10@gmail.com>
2025-01-16 20:23:22 +0900

    refactor: Link 경로 수정

--- src/pages/Driver/DriverMainPage.tsx
+++ src/pages/Driver/DriverMainPage.tsx
@@ -16,19 +16,13 @@ const DriverMainPage = () => {
               <h2 className="text-2xl md:text-4xl font-semibold">
                 운영 시간표
               </h2>
-              <Link
-                to="DriverSchedulePage"
-                className="mt-4 text-sm md:text-base"
-              >
+              <Link to="/" className="mt-4 text-sm md:text-base">
                 바로가기 &gt;&gt;
               </Link>
             </div>
             <div className="bg-yellow-400 text-white rounded-lg w-72 h-56 md:w-80 md:h-64 flex flex-col items-center justify-center hover:scale-105 transition">
               <h2 className="text-2xl md:text-4xl font-semibold">출석 체크</h2>
-              <Link
-                to="AttendanceCheckPage"
-                className="mt-4 text-sm md:text-base"
-              >
+              <Link to="/" className="mt-4 text-sm md:text-base">
                 바로가기 &gt;&gt;
               </Link>
             </div>

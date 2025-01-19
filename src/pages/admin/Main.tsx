429d5ba2e3cf34fa23b13b5c3741cfa30ebf9c59
gunyongbok <qhrrjsdyd123@gmail.com>
2025-01-19 11:32:12 +0900

    feat: 관리자 메인페이지 디지인 완료

--- src/pages/admin/Main.tsx
+++ src/pages/admin/Main.tsx
@@ -1,12 +1,20 @@
-import React from "react";
 import FullScreenContainer from "../../components/container/FullScreenContainer";
 import Header from "../../components/Header";
+import android from "../../assets/img/admin/android_admin_main.png";
+import intro from "../../assets/img/admin/intro_main.png";
 
 const Main = () => {
   return (
     <FullScreenContainer>
       <Header />
-      <div className="w-full h-[691px] bg-primary"></div>
+      <div className="w-full h-[691px] bg-primary relative">
+        <img className="absolute top-60 left-20" src={intro} alt="intro" />
+        <img
+          className="h-[858px] w-[410px] absolute right-[300px] top-20"
+          src={android}
+          alt="android-main"
+        />
+      </div>
     </FullScreenContainer>
   );
 };

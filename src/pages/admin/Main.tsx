337afa2cea34ce7bc57b9ad7a9c23015d755bff2
gunyongbok <qhrrjsdyd123@gmail.com>
2025-01-19 11:10:07 +0900

    feat: 메인 컬러 변수화

--- src/pages/admin/Main.tsx
+++ src/pages/admin/Main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import FullScreenContainer from "../../components/container/FullScreenContainer";
+import Header from "../../components/Header";
 
 const Main = () => {
-  return <FullScreenContainer></FullScreenContainer>;
+  return (
+    <FullScreenContainer>
+      <Header />
+    </FullScreenContainer>
+  );
 };
 
 export default Main;

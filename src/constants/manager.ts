707b898079ed4dc772db4026821787bddc673d63
Youjin Kim <gopar0101@gmail.com>
2025-01-21 18:09:08 +0900

    메뉴 아이템 분리

---
+++ src/constants/manager.ts
@@ -0,0 +1,14 @@
+export const MENU_ITEMS = [
+  {
+    path: '/admin/students',
+    label: '학생 관리'
+  },
+  {
+    path: '/admin/shuttles',
+    label: '셔틀 관리'
+  },
+  {
+    path: '/admin/vehicles',
+    label: '차량 관리'
+  }
+]; 

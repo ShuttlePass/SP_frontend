4f4e47bf4de88fa6e56364eb924fc39ef608d3b3
kimywann <Kimywan10@gmail.com>
2025-02-02 19:28:06 +0900

    feat: 로그인 상태를 localStorage에 저장

--- src/pages/admin/SignIn.tsx
+++ src/pages/admin/SignIn.tsx
@@ -26,6 +26,7 @@ const SignIn = () => {
       api.post("/user/login", newUser),
     onSuccess: () => {
       alert("로그인이 완료되었습니다.");
+      localStorage.setItem("isLoggedIn", "true");
       setFormValues({
         us_id: "",
         us_password: "",

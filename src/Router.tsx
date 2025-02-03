86ddbd71483ab97660f1cf0b3e313a45f99b1f36
Youjin Kim <gopar0101@gmail.com>
2025-02-03 21:11:40 +0900

    라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/admin/Main";
 import Signup from "./pages/admin/Signup";
+import StudentPage from "./pages/manager/StudentPage";
+import ShuttlePage from "./pages/manager/ShuttlePage";
+import VehiclePage from "./pages/manager/VehiclePage";
 
 const Router: React.FC = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/admin/students" element={<StudentPage />} />
+      <Route path="/admin/shuttles" element={<ShuttlePage />} />
+      <Route path="/admin/vehicles" element={<VehiclePage />} />
+    </Routes>
   );
 };
 
